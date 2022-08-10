2948c7235694c942ed4ca74c26f10cac97203e40
yira <dladlfk1@gmail.com>
2022-08-10 18:37:31 +0900

    first React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import Button from "./Button";
+import styles from "./h1.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className={styles.title}>Hello First React</h1>
+      <Button text={"save"} />
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+import React from "react";
+import styles from "./Button.module.css";
+
+const Button = ({ text }) => {
+  return <button className={styles.btn}>{text}</button>;
+};
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default Button;
