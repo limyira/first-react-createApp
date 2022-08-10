00139b3f6ec5a03692587e88ccf0d4895fa4f230
yira <dladlfk1@gmail.com>
2022-08-10 19:00:22 +0900

    deps

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const Search = (event) => {
+    setKeyword(event.target.value);
+  };
   const setCount = () => {
     setCounter((prev) => prev + 1);
   };
@@ -10,11 +14,24 @@ function App() {
     console.log("Hello");
   };
   useEffect(helloOnce, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length >= 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
+
   return (
     <div>
       <h1>Hello First React</h1>
       <h2>{counter}</h2>
+      <input
+        value={keyword}
+        onChange={Search}
+        placeholder="Search now"
+        type="text"
+      ></input>
       <button onClick={setCount}>Click now</button>
+      <h6>{keyword}</h6>
     </div>
   );
 }
